ef750656d31b4a2311008f616359922004bed815
Backbiter99 <52291923+Backbiter99@users.noreply.github.com>
2019-07-04 19:45:27 +0530

    Delete Third.py

--- Attempted solutions/Third.py
+++
@@ -1,6 +0,0 @@
-a = float(input('Value of the bitcoin at the time of purchase: '))
-b = float(input('Percentage increase in the value of bitcoin: '))
-
-c = a * b / 100
-
-print('Total value of bitcoin:',a+c,'\nIncrease in the value of bitcoin:',c)
